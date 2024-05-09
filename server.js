95601b715edf282880015f09797311d6c19fa8da
JDIdeas <154945047+JDIdeas@users.noreply.github.com>
2024-05-09 16:54:06 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { Server: SocketServer } = require('socket.io');
+const { createServer } = require('http');
+const mongoose = require('mongoose');
+
+const app = express();
+const httpServer =  createServer(app);
+const io =  new SocketServer(httpServer, {
+    cors: {
+        origin: true,
+        credentials: true,
+    },
+});
+
+app.get('/', (req, res) =>{
+    res.send("Server is Running")
+})
+
+io.on('connection', (socket) =>{
+    socket.on('pingServer', () =>{
+        socket.emit('pingClient', `Recived from Server on ${Date()}`)
+    })
+})
+
+const PORT = 80;
+
+app.listen(PORT)
